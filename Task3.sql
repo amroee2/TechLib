fe41cd476e9ee99f5e9bedde3a9a14892a32a534
Amro Qadadha <95709028+amroee2@users.noreply.github.com>
2024-10-01 12:49:18 +0300

    Add Task3

---
+++ Task3.sql
@@ -0,0 +1,15 @@
+SELECT 
+    Borrowers.BorrowerId, 
+    Borrowers.[First Name], 
+    Borrowers.[Last Name], 
+    COUNT(Borrowers.BorrowerId) AS NumberOfLoans, 
+    RANK() OVER(ORDER BY COUNT(Borrowers.BorrowerId) DESC) AS FreqRank
+FROM 
+    Borrowers
+JOIN 
+    Loans
+    ON Borrowers.BorrowerId = Loans.BorrowerId
+GROUP BY 
+    Borrowers.BorrowerId, Borrowers.[First Name], Borrowers.[Last Name]
+ORDER BY 
+    NumberOfLoans DESC;
