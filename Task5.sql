4728b8df922c9fbdc0fa9534ea54cc45b99b319a
Amro Qadadha <95709028+amroee2@users.noreply.github.com>
2024-10-07 10:21:03 +0300

    Update Task5.sql

--- Task5.sql
+++ Task5.sql
@@ -1,11 +1,10 @@
-
 CREATE OR ALTER PROCEDURE insertBorrower (
-
     @First_Name    VARCHAR(50),
     @Last_Name     VARCHAR(50),
     @Email         VARCHAR(50),
     @DateOfBirth   DATE,
-    @MembershipDate DATE
+    @MembershipDate DATE,
+    @NewBorrowerID INT OUTPUT
 )
 AS
 BEGIN
@@ -16,12 +15,25 @@ BEGIN
     )
     BEGIN
         PRINT 'Error: Email already exists in the database.';
-        RETURN;
+        SET @NewBorrowerID = NULL;
+        RETURN -1;
     END
+
     INSERT INTO Borrowers([First Name], [Last Name], Email, [Date Of Birth], [Membership Date])
     VALUES (@First_Name, @Last_Name, @Email, @DateOfBirth, @MembershipDate);
 
+    SET @NewBorrowerID = SCOPE_IDENTITY();
     PRINT 'Borrower successfully inserted';
+    RETURN 0;
 END;
 
-EXEC insertBorrower "Test", "Test", "amroqadadha4@gmail.com", '2002-05-18', '2024-08-4';
+DECLARE @NewID INT;
+
+EXEC insertBorrower 'Test', 'Test', 'amroqadadha700@gmail.com', '2002-05-18', '2024-08-04', @NewBorrowerID = @NewID OUTPUT;
+
+IF @NewID IS NOT NULL
+BEGIN
+    SELECT *
+    FROM Borrowers
+    WHERE BorrowerId = @NewID;
+END
